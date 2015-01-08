026827409d814916efc32c7ea44af4ef210cf924
Spencer Carli <spencercarli@gmail.com>
2015-01-08 12:44:25 -0500

    Initial commit (working)

---
+++ mobile-config.js
@@ -0,0 +1,9 @@
+App.info({
+  name: 'Seesaw',
+  id: 'com.differential.seesawtest',
+  description: 'Seesaw is a family social network that makes it simple to connect with other families.',
+  author: 'Seesaw',
+  email: 'info@goseesaw.io',
+  website: 'http://goseesaw.io',
+  version: '0.1.0'
+});
